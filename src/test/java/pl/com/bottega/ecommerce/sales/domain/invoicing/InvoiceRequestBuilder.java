09a461a3dd0f2f88697090e118c54ed0eb24a890
Piotr Weglewski <190160@edu.p.lodz.pl>
2016-05-04 20:49:22 +0200

    Added invoice request builder

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceRequestBuilder.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceRequestBuilder.java
@@ -1,7 +1,39 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by piotr on 04.05.2016.
  */
 public class InvoiceRequestBuilder {
+    private static final String DEFAULT_CLIENT_NAME = "John";
+    private final Id id = Id.generate();
+
+    private ClientData clientData;
+
+    private List<RequestItem> items = new ArrayList<>();
+
+    public InvoiceRequestBuilder() {
+        clientData = new ClientData(id, DEFAULT_CLIENT_NAME);
+    }
+
+    public InvoiceRequestBuilder setNewClientData(ClientData clientData){
+        this.clientData = clientData;
+        return this;
+    }
+
+    public InvoiceRequestBuilder addItem(RequestItem requestItem){
+        this.items.add(requestItem);
+        return this;
+    }
+
+    public InvoiceRequest build(){
+        final InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+        items.stream().forEach(invoiceRequest::add);
+        return invoiceRequest;
+    }
 }
